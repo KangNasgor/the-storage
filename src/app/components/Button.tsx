1c2ef7e57da87f48760a9207580f4e0fde004e58
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2026-01-07 21:34:17 +0700

    changing table's color theme

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,11 +1,15 @@
 export default function Button({ text, background, color } : { text : string, background : string, color : string }) {
+    const backgroundColor : string = background.trim();
+    const textColor : string = text.trim();
     return(
-        <div className='bg-white rounded-3xl px-5 py-3 cursor-pointer active:scale-90 transition-transform duration-100' style={{
-            backgroundColor : background
+        <div className={`${backgroundColor.startsWith('bg-') ? backgroundColor : ''} rounded-3xl px-5 py-3 cursor-pointer active:scale-90 transition-transform duration-100`} style={{
+            backgroundColor : backgroundColor.startsWith('bg-') ? undefined : backgroundColor
         }}>
-            <h1 style={{
-                color: color,
+            <h1 className={`${textColor.startsWith('text') ? textColor : ''}`} style={{
+                color: textColor.startsWith('text') ? undefined : textColor,
             }}>{text}</h1>
         </div>
     )
 }
+
+// Provide background/text color in tailwind way or hexadecimal
--- src/app/components/Items.tsx
+++ src/app/components/Items.tsx
@@ -7,35 +7,57 @@ export default async function Items(){
         name : string,
         amount : number,
         category : string,
+        created_at : string,
+        updated_at : Date,
     }
 
     const items : Items[] = await getItems();
 
     return(
-        <table className='table table-auto text-center'>
-            <thead className='bg-gray-500'>
-                <tr>
-                    <td className='px-5 py-3'>ID</td>
-                    <td className='px-5 py-3'>Name</td>
-                    <td className='px-5 py-3'>Amount</td>
-                    <td className='px-5 py-3'>Category</td>
-                    <td className='px-5 py-3'>Options</td>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map(item => (
-                    <tr key={item.id} className='bg-gray-600'>
-                        <td className='px-5 py-3'>{item.id}</td>
-                        <td className='px-5 py-3'>{item.name}</td>
-                        <td className='px-5 py-3'>{item.amount}</td>
-                        <td className='px-5 py-3'>{item.category}</td>
-                        <td className='px-5 py-3 flex flex-row gap-5'>
-                            <Button text='Edit' background='#fffff' color='black'/>
-                            <Button text='Delete' background='#fffff' color='black'/>
-                        </td>
+        <div className='grid grid-cols-1 gap-5'>
+            <div className='flex flex-row gap-5'>
+                <Button text='Create' background='bg-sky-600' color='black'/>
+                <Button text='History' background='bg-sky-600' color='black'/>
+            </div>
+            <table className='table table-auto text-center'>
+                <thead className='bg-sky-900'>
+                    <tr>
+                        <td className='px-5 py-3'>ID</td>
+                        <td className='px-5 py-3'>Name</td>
+                        <td className='px-5 py-3'>Amount</td>
+                        <td className='px-5 py-3'>Category</td>
+                        <td className='px-5 py-3'>Creation Date</td>
+                        <td className='px-5 py-3'>Date of Update</td>
+                        <td className='px-5 py-3'>Options</td>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {items.map(item => (
+                        <tr key={item.id} className='bg-sky-950'>
+                            <td className='px-5 py-3'>{item.id}</td>
+                            <td className='px-5 py-3'>{item.name}</td>
+                            <td className='px-5 py-3'>{item.amount}</td>
+                            <td className='px-5 py-3'>{item.category}</td>
+                            <td className='px-5 py-3'>
+                                {new Intl.DateTimeFormat("id-ID", {
+                                    dateStyle: 'medium',
+                                    timeStyle: "long",
+                                }).format(new Date(item.created_at))}
+                            </td>
+                            <td className='px-5 py-3'>
+                                {new Intl.DateTimeFormat("id-ID", {
+                                    dateStyle: 'medium',
+                                    timeStyle: 'long',
+                                }).format(new Date(item.updated_at))}
+                            </td>
+                            <td className='px-5 py-3 flex flex-row gap-5'>
+                                <Button text='Edit' background='bg-sky-600' color='white'/>
+                                <Button text='Delete' background='bg-sky-600' color='black'/>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
