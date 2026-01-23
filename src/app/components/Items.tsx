9381518b6dc08408d3032f8918ae75dd09d7a178
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2026-01-23 22:34:20 +0700

    jiman

--- src/app/components/Items.tsx
+++ src/app/components/Items.tsx
@@ -1,8 +1,10 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { getItems } from '../../../lib/getItems';
 import Button from './Button';
 import CreateRecord from './CreateRecord';
 import DeleteRecord from './DeleteRecord';
 import UpdateRecord from './UpdateRecord';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default async function Items(){
@@ -20,13 +22,25 @@ export default async function Items(){
     return(
         <div className='grid grid-cols-1 gap-5'>
             <div className='flex flex-row gap-5'>
-                <CreateRecord />
-                <Button text='History' background='bg-sky-600' color='white'/>
             </div>
-            <table className='table table-auto text-center'>
+            <table className='table table-auto rounded-md overflow-hidden text-center'>
                 <thead className='bg-sky-900'>
+                    <tr className='bg-sky-700 '>
+                        <td colSpan={3}>
+                            <div className='flex flex-row gap-3 justify-start w-full px-3'>
+                                <CreateRecord />
+                                <Button text='History' background='bg-sky-600' color='white'/>
+                            </div>
+                        </td>
+                        <td colSpan={3} className='py-2 px-3'>
+                            <div className='flex flex-row items-center gap-3 w-full justify-end'>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} className='w-5 h-5'/>
+                                <input className='rounded-md px-3 py-2 text-black bg-white' placeholder='mouse'/>
+                                <Button text='Search' background='bg-sky-600' color='white'/>
+                            </div>
+                        </td>
+                    </tr>
                     <tr>
-                        <td className='px-5 py-3'>ID</td>
                         <td className='px-5 py-3'>Name</td>
                         <td className='px-5 py-3'>Amount</td>
                         <td className='px-5 py-3'>Category</td>
@@ -38,7 +52,6 @@ export default async function Items(){
                 <tbody>
                     {items.map(item => (
                         <tr key={item.id} className='bg-sky-950'>
-                            <td className='px-5 py-3'>{item.id}</td>
                             <td className='px-5 py-3'>{item.name}</td>
                             <td className='px-5 py-3'>{item.amount}</td>
                             <td className='px-5 py-3'>{item.categorys.name}</td>
